416db07fc65719d3750fd25a85ed4843c2957bce
KG0312 <104473007+KG0312@users.noreply.github.com>
2022-04-26 10:48:30 -0600

    28-04-trabajo-en-clases

---
+++ index4.js
@@ -0,0 +1,7 @@
+var me_invitaron;
+
+me_invitaron = prompt('ehhh si me invitaron');
+me_invitaron = prompt('que mal no me invitaron');
+
+console.log('ehhh si me invitaron')
+console.log('que mal no me invitaron')
