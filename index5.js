d636a0aa914d3efe9b0e43de33753171f3471422
KG0312 <104473007+KG0312@users.noreply.github.com>
2022-04-26 10:08:04 -0600

    28-04-trabajo-en-clases

---
+++ index5.js
@@ -0,0 +1,6 @@
+function multiplica_de_tres_numeros (numero1,numero2,numero3){
+        (multiplica_de_tres_numeros)
+    console.log(numero1*numero2*numero3)
+}
+
+multiplica_de_tres_numeros(5,7,9);
